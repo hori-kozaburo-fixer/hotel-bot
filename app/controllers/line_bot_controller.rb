c5c6b429eb3f7dc2d0b9bcfde53ac73d37d1477a
zab3 <xelvis03@gmail.com>
2020-12-04 09:54:01 +0900

    Lineから情報を受け取れるように設定

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,15 +1,23 @@
 class LineBotController < ApplicationController
-
-  protect_from_forgery except:[:callback]
+  protect_from_forgery except: [:callback]
 
   def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+       p '不正なリクエストです。'
+      return head :bad_request
+    end
+     p '正しいリクエストです。'
   end
 
-  private
-  def client
-    @client || = Line::Bot::Client.new {|config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
+   private
+ 
+     def client
+       @client ||= Line::Bot::Client.new { |config|
+         config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+         config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+       }
+       
+     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'hello#index'
-  post '/callback' => 'line_bot#create'
+  get 'hello/index'
+  post '/callback' => 'line_bot#callback'
 end
